d6f6ee6685c93449d2c484cd77f65a321f9d6d8b
syulyul <syul9800@gmail.com>
2023-01-31 13:25:23 +0900

    Intermediate Commit

--- src/main/java/org/zerock/b01/controller/MemberController.java
+++ src/main/java/org/zerock/b01/controller/MemberController.java
@@ -6,13 +6,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.b01.dto.MemberJoinDTO;
+import org.zerock.b01.service.MemberService;
 
 @Controller
 @RequestMapping("/member")
 @Log4j2
 @RequiredArgsConstructor
 public class MemberController {
+    //의존성 주입
+    private final MemberService memberService;
 
     @GetMapping("/login")
     public void loginGET(String errorCode, String logout) {
@@ -32,12 +36,22 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String joinPOST(MemberJoinDTO memberJoinDTO){
+    public String joinPOST(MemberJoinDTO memberJoinDTO, RedirectAttributes redirectAttributes){
 
         log.info("join post...");
         log.info(memberJoinDTO);
 
-        return "redirect:/board/list";
+        try {
+            memberService.join(memberJoinDTO);
+        } catch (MemberService.MidExistException e) {
+
+            redirectAttributes.addFlashAttribute("error", "mid");
+            return "redirect:/member/join";
+        }
+
+        redirectAttributes.addFlashAttribute("result", "success");
+
+        return "redirect:/member/login"; //회원 가입 후 로그인
     }
 
 }
